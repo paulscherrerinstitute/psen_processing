2e3c45cbe1018be7d9fbc242a8d3a4c98b4fcbce
Andrej Babic <andrej.babic@psi.ch>
2018-05-09 10:38:53 +0200

    Add processor stub

--- psen_processing/processor.py
+++ psen_processing/processor.py
@@ -1,2 +1,21 @@
+from logging import getLogger
+
+_logger = getLogger(__name__)
+
+
 def get_stream_processor(input_stream, output_stream_port, bs_data_prefix):
-    return None
+
+    def stream_processor(running_flag, roi_signal, roi_background):
+        try:
+            # TODO: Connect to the stream, open output stream.
+
+            running_flag.set()
+
+            while running_flag.is_set():
+                pass
+
+        except Exception as e:
+            _logger.error("Error while processing the stream. Exiting. Error: ", e)
+            running_flag.clear()
+
+    return stream_processor
