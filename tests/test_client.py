9a629b6225e58f43a78b62c7d413322cacbee682
Andrej Babic <andrej.babic@psi.ch>
2018-05-09 17:37:50 +0200

    Add test to change ROI on the fly

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,7 +3,7 @@ from time import sleep
 
 import numpy
 from bsread.sender import sender
-from bsread import source, PULL
+from bsread import source, PULL, json
 from multiprocessing import Process
 
 from psen_processing import config, PsenProcessingClient
@@ -16,10 +16,10 @@ class TestClient(unittest.TestCase):
         self.n_images = 5
         self.pv_name_prefix = "JUST_TESTING"
 
-        image = numpy.zeros(shape=(1024, 1024), dtype="uint16")
-        image += 1
+        self.image = numpy.zeros(shape=(1024, 1024), dtype="uint16")
+        self.image += 1
 
-        data_to_send = {self.pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE: image}
+        data_to_send = {self.pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE: self.image}
 
         def send_data():
             with sender(port=11000) as output_stream:
@@ -107,3 +107,43 @@ class TestClient(unittest.TestCase):
 
         client.stop()
         self.assertEqual(client.get_status(), "stopped")
+
+    def test_change_roi_while_running(self):
+        client = PsenProcessingClient("http://localhost:10000/")
+
+        roi_signal = [0, 1024, 0, 1024]
+        client.set_roi_signal(roi_signal)
+
+        roi_background = [0, 100, 0, 100]
+        client.set_roi_background(roi_background)
+
+        client.start()
+
+        processed_data = []
+
+        with source(host="localhost", port=12000, mode=PULL, receive_timeout=1000) as input_stream:
+            for index in range(self.n_images):
+                processed_data.append(input_stream.receive())
+
+        updated_roi_signal = [100, 200, 100, 200]
+        client.set_roi_signal(updated_roi_signal)
+
+        data_to_send = {self.pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE: self.image}
+
+        with sender(port=11000) as output_stream:
+            for x in range(self.n_images):
+                output_stream.send(data=data_to_send)
+
+        with source(host="localhost", port=12000, mode=PULL, receive_timeout=1000) as input_stream:
+            for index in range(self.n_images):
+                processed_data.append(input_stream.receive())
+
+        client.stop()
+
+        processing_parameters_name = self.pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE + "_processing_parameters"
+
+        start_processing_parameters = json.loads(processed_data[0].data.data[processing_parameters_name].value)
+        end_processing_parameters = json.loads(processed_data[9].data.data[processing_parameters_name].value)
+
+        self.assertListEqual(roi_signal, start_processing_parameters["roi_signal"])
+        self.assertListEqual(updated_roi_signal, end_processing_parameters["roi_signal"])
