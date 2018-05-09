e2f5e8edd1c7b502384b432ac1964d727dbeb11f
Andrej Babic <andrej.babic@psi.ch>
2018-05-09 10:38:38 +0200

    Add thread management

--- psen_processing/manager.py
+++ psen_processing/manager.py
@@ -1,14 +1,59 @@
+from threading import Event, Thread
+
+from logging import getLogger
+
+from psen_processing import config
+
+_logger = getLogger(__name__)
+
+
 class ProcessingManager(object):
 
-    def __init__(self, stream_processor, auto_start=False):
-        self.processor = stream_processor
+    def __init__(self, stream_processor, roi_signal=None, roi_background=None, auto_start=False):
+
+        self.stream_processor = stream_processor
         self.auto_start = auto_start
 
+        if roi_background is None:
+            roi_background = config.DEFAULT_ROI_BACKGROUND
+        self.roi_background = roi_background
+
+        if roi_signal is None:
+            roi_signal = config.DEFAULT_ROI_SIGNAL
+        self.roi_signal = roi_signal
+
+        self.processing_thread = None
+        self.running_flag = None
+
+        if auto_start:
+            self.start()
+
     def start(self):
-        pass
+
+        if self._is_running():
+            _logger.debug("Trying to start an already running stream_processor.")
+            return
+
+        self.running_flag = Event()
+
+        self.processing_thread = Thread(target=self.stream_processor,
+                                        args=(self.running_flag, self.roi_signal, self.roi_background))
+
+        self.processing_thread.start()
+
+        if not self.running_flag.wait(timeout=config.PROCESSOR_START_TIMEOUT):
+            self.stop()
+
+            raise RuntimeError("Cannot start processing thread in time. Please check error log for more info.")
 
     def stop(self):
-        pass
+
+        if self._is_running():
+            self.running_flag.clear()
+            self.processing_thread.join()
+
+        self.processing_thread = None
+        self.running_flag = None
 
     def set_roi(self, roi_index, roi_config):
         pass
@@ -19,5 +64,11 @@ class ProcessingManager(object):
     def get_statistics(self):
         pass
 
+    def _is_running(self):
+        return self.processing_thread and self.processing_thread.is_alive()
+
     def get_status(self):
-        pass
+        if self._is_running():
+            return "processing"
+        else:
+            return "stopped"
