047918bae4834c0a37e56df0dbbde9a43fa63af0
Andrej Babic <andrej.babic@psi.ch>
2018-05-11 13:33:58 +0200

    New library version
    - Change default REST port.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="psen_processing",
-      version="1.0.1",
+      version="1.0.2",
       author="Paul Scherrer Institute",
       description="PSEN image processing.",
       license="GPL3",
