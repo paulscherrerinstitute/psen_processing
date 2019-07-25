87638ded47db25c761d1be7df4bac398f3118325
Andrej Babic <andrej.babic@psi.ch>
2019-07-25 11:19:49 +0200

    Move docker build to dockerhub

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 VERSION=1.2.0
-docker build --no-cache=true -t docker.psi.ch:5000/psen_processing .
-docker tag docker.psi.ch:5000/psen_processing docker.psi.ch:5000/psen_processing:$VERSION
-docker push docker.psi.ch:5000/psen_processing:$VERSION
-docker push docker.psi.ch:5000/psen_processing
+
+docker build --no-cache=true -t paulscherrerinstitute/psen_processing .
+docker tag paulscherrerinstitute/psen_processing paulscherrerinstitute/psen_processing:$VERSION
+docker login
+docker push paulscherrerinstitute/psen_processing:$VERSION
+docker push paulscherrerinstitute/psen_processing
